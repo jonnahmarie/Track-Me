440f571a7eeb723e26f7accb17421831553fa482
jonnahmarie <jonnah.marie@gmail.com>
2021-01-19 16:53:05 -0800

    updated seed file

--- db/seeds.sql
+++ db/seeds.sql
@@ -6,17 +6,8 @@ VALUES ("Engineering");
 INSERT INTO role (title, salary, department_id)
 VALUES ("Software Engineer", 90000.00, 1);
 
-INSERT INTO employee (first_name, last_name, role_id, manager_id)
-VALUES ("John", "Doe", 1, 2);
-
-INSERT INTO department (name)
-VALUES ("Engineering");
-
-INSERT INTO role (title, salary, department_id)
-VALUES ("Lead Engineer", 120000.00, 2);
-
 INSERT INTO employee (first_name, last_name, role_id)
-VALUES ("Diane", "Hernandez", 2);
+VALUES ("John", "Doe", 1);
 
 SELECT * FROM department;
 SELECT * FROM role;
